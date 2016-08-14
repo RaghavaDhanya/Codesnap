44d0f60489884cadc559d938bab9e0c44a6c864a
RaghavaDhanya <raghava.dhanya@gmail.com>
2016-08-14 13:49:13 +0530

    now clipboard is being saved! added css to popup, list of last 10 copy in popup!

--- core.js
+++ core.js
@@ -11,7 +11,7 @@ for(var i in list)
 
   var textnode = document.createTextNode("Copy");
   button.appendChild(textnode);
-  if(list[i].parentNode!=null)
+  if(list[i].parentNode!=null && list[i].hasChildNodes())
   list[i].parentNode.insertBefore(button,list[i]);
 }
 function copyclick(ele)
@@ -25,10 +25,38 @@ function copyclick(ele)
     var successful = document.execCommand('copy');  
     var msg = successful ? 'successful' : 'unsuccessful';  
     console.log('Copy ' + msg);  
+    saveLocal(ele.textContent);
   } 
   catch(err) 
   {  
     console.log('unable to copy');  
   }    
   window.getSelection().removeAllRanges();  
+}
+function saveLocal(text)
+{ 
+  var date= new Date();
+  var datestr=date.getDate()+" "+(date.getMonth()+1)+" "+date.getFullYear()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+  chrome.storage.sync.get({"clip":"none"},function(items){
+    if(items["clip"].toString()=="none")
+    { 
+
+      var clip=[{code:text,timecopy:datestr}];
+      chrome.storage.sync.set({ "clip": clip }, function(){
+        console.log("saved");
+      });
+    }
+    else
+    {
+      var arr=items["clip"];
+      if(arr.unshift({code:text,timecopy:datestr})>10)
+        {
+          arr.pop();
+        }
+      chrome.storage.sync.set({ "clip": arr }, function(){
+        console.log("saved");
+      });
+    }
+    });
+ 
 }
--- popup.js
+++ popup.js
@@ -5,19 +5,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
   }, false);
 
+chrome.storage.sync.get({"clip":"none"},function(items){
+    if(items["clip"].toString()!="none")
+    { 
+      var data=items["clip"];
+      console.log(data);
+      var myList= document.getElementById('mylist');
+      for(var j=0;j<data.length;j++)
+      {
+        li=document.createElement("div");
+        span=document.createElement("span");
+        span.onclick=function(){console.log("span");};
+        span.id="list";
+        span.appendChild(document.createTextNode((data[j]).timecopy.toString()));
+        li.appendChild(span);
+        b=document.createElement("button");
+        b.appendChild(document.createTextNode("copy"));
+        li.appendChild(b);
+        myList.appendChild(li);
 
+      }
+      for(var i=data.length;i<10;i++)
+      {
+        li=document.createElement("div");
+        li.appendChild(document.createTextNode("Empty"));
+        myList.appendChild(li);
+      } 
+    }});
 
-var myList= document.getElementById('mylist');
-for(var i=0;i<10;i++)
-{
-  li=document.createElement("div");
-  li.appendChild(document.createTextNode(i.toString()));
-  b=document.createElement("button");
-  b.appendChild(document.createTextNode(i.toString()));
-  li.appendChild(b);
-  li.display="block";
-  myList.appendChild(li);
-
-}
 }, false);
 
